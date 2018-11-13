830ebff7dadb189068208cd59b31819bacd2fc12
pengzengyi <peng123>
2018-11-13 22:41:07 +0800

    提交

--- src/main/java/com/soecode/lyf/web/BookController.java
+++ src/main/java/com/soecode/lyf/web/BookController.java
@@ -31,25 +31,42 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	/*
+value的值是数组，可以将多个url映射到同一个方法:
+@RequestMapping({
+			"/request1",
+			"/request2",
+			"/request3"
+	})*/
+	/*@RequestMapping(value = "/list", method = RequestMethod.GET)
 	private String list(Model model) {
 		List<Book> list = bookService.getList();
 		model.addAttribute("list", list);
 		// list.jsp + model = ModelAndView
 		return "list";// WEB-INF/jsp/"list".jsp
-	}
+	}*/
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    private Result<List> list() {
+        List<Book> list = bookService.getList();
+        Result<List> listResult = new Result<>();
+        listResult.setData(list);
+        listResult.setSuccess(true);
+        return listResult;
+    }
 
 	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
-	private String detail(@PathVariable("bookId") Long bookId, Model model) {
-		if (bookId == null) {
-			return "redirect:/book/list";
-		}
+    @ResponseBody
+	private Result<Book> detail(@PathVariable("bookId") Long bookId, Model model) {
+        Result<Book> bookResult = new Result<>();
 		Book book = bookService.getById(bookId);
 		if (book == null) {
-			return "forward:/book/list";
+			return bookResult;
 		}
-		model.addAttribute("book", book);
-		return "detail";
+        bookResult.setData(book);
+		bookResult.setSuccess(true);
+		return bookResult;
 	}
 
 	// ajax json
@@ -73,4 +90,24 @@ public class BookController {
 		return new Result<AppointExecution>(true, execution);
 	}
 
+	/*使用@RequestParam常用于处理简单类型的绑定。
+	value：参数名字，即入参的请求参数名字，如value=“item_id”表示请求的参数区中的名字为item_id的参数的值将传入；
+	required：是否必须，默认是true，表示请求中一定要有相应的参数，否则将报；
+	TTP Status 400 - Required Integer parameter 'XXXX' is not present
+	defaultValue：默认值，表示如果请求中没有同名参数时的默认值
+*/
 }
+
+
+/*
+		在controller方法形参上可以定义request和response，使用request或response指定响应结果：
+		1、使用request转向页面，如下：
+		request.getRequestDispatcher("页面路径").forward(request, response);
+
+		2、也可以通过response页面重定向：
+		response.sendRedirect("url")
+
+		3、也可以通过response指定响应结果，例如响应json数据如下：
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("application/json;charset=utf-8");
+		response.getWriter().write("json串");*/
